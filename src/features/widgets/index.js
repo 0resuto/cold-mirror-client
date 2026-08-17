11f49fc3e4eab40480a8f6da7105ae2c21187165
0resuto <pilot.ua4@gmail.com>
2026-08-17 18:33:06 +0300

    fix(widgets): map missing and incorrectly named components in registry
    - Added radar widget mapping to LiveRadar
    - Corrected trackmap to LinearTrackMap
    - Corrected weather to LiveWeather
    - Corrected pit to PitHelper
    - Corrected dash to DigitalDash

--- src/features/widgets/index.js
+++ src/features/widgets/index.js
@@ -6,9 +6,10 @@ componentRegistry.register('standings', React.lazy(() => import('cold-mirror-wid
 componentRegistry.register('relative', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LiveRelative }))));
 componentRegistry.register('fuel', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LiveFuel }))));
 componentRegistry.register('inputs', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LiveInputs }))));
-componentRegistry.register('trackmap', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.TrackMap }))));
-componentRegistry.register('weather', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.Weather }))));
-componentRegistry.register('pit', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.PitStops }))));
-componentRegistry.register('dash', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.DashboardDashboard }))));
+componentRegistry.register('radar', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LiveRadar }))));
+componentRegistry.register('trackmap', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LinearTrackMap }))));
+componentRegistry.register('weather', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.LiveWeather }))));
+componentRegistry.register('pit', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.PitHelper }))));
+componentRegistry.register('dash', React.lazy(() => import('cold-mirror-widgets').then(module => ({ default: module.DigitalDash }))));
 
 export { componentRegistry };
